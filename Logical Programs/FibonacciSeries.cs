6259f986dd88db7392377dc154a0d13d79db9ed3
Akshata72 <sableakshata880@gmail.com>
2022-03-14 20:00:18 +0530

    Added:UC1-Fibonacci_Series

---
+++ Logical Programs/FibonacciSeries.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+
+        public void Fibonacciseries()
+        {
+            int Number1 = 0;
+            int Number2 = 1;
+            int Number3;
+            Console.WriteLine("Enter the Number of Elements:");
+            int Number = int.Parse(Console.ReadLine());
+            for (int i = 1; i < Number; i++)
+            {
+                Number3 = Number1 + Number2;
+                Console.WriteLine(Number3);
+                Number1 = Number2;
+                Number2 = Number3;
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
---
+++ Logical Programs/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace LogicalPrograms
+{
+    class LogicalPrograms 
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 1 For Fibonacci Series:");
+            int Option = int.Parse(Console.ReadLine());
+
+
+
+            switch(Option)
+            {
+                case 1:
+                    FibonacciSeries fibonacciseries = new FibonacciSeries();
+                    fibonacciseries.Fibonacciseries();
+                    break;
+            }
+
+
+
+
+        }
+
+
+    }
+
+}
